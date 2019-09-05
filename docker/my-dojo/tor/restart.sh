00d800f72d6671dc4f82a81ad955c68ee7f4f67b
kenshin-samourai <kenshin_samourai@tutanota.com>
2019-09-05 16:39:20 +0200

    replace torrc file by command lines arguments set in restart script. restructure docker-tor.conf for homogeneity. define env vars in Dockerfile for easier tuning (for pi4 and others platforms)

--- docker/my-dojo/tor/restart.sh
+++ docker/my-dojo/tor/restart.sh
@@ -6,4 +6,31 @@ echo "## Set permissions on /var/lib/tor dir ###"
 chmod 750 /var/lib/tor
 
 echo "## Start tor #############################"
-tor
+
+tor_options=(
+  --SocksPort 172.28.1.4:9050
+  --SocksPolicy "accept 172.28.0.0/16"
+  --SocksPolicy "reject *"
+  --DataDirectory /var/lib/tor/.tor
+  --DataDirectoryGroupReadable 1
+  --HiddenServiceDir /var/lib/tor/hsv2dojo
+  --HiddenServiceVersion 2
+  --HiddenServicePort "80 172.29.1.3:80"
+  --HiddenServiceDir /var/lib/tor/hsv3dojo
+  --HiddenServiceVersion 3
+  --HiddenServicePort "80 172.29.1.3:80"
+  --HiddenServiceDir /var/lib/tor/hsv2bitcoind
+  --HiddenServiceVersion 2
+  --HiddenServicePort "8333 172.28.1.5:8333"
+  --HiddenServiceDirGroupReadable 1
+)
+
+if [ "$TOR_USE_BRIDGES" == "on" ]; then
+  tor_options+=(--ClientTransportPlugin "obfs4 exec /usr/local/bin/obfs4proxy")
+  tor_options+=(--UseBridges 1)
+  tor_options+=(--Bridge "obfs4 $TOR_BRIDGE_1_IP_PORT $TOR_BRIDGE_1_ID cert=$TOR_BRIDGE_1_CERT iat-mode=$TOR_BRIDGE_1_IAT_MODE")
+  tor_options+=(--Bridge "obfs4 $TOR_BRIDGE_2_IP_PORT $TOR_BRIDGE_2_ID cert=$TOR_BRIDGE_2_CERT iat-mode=$TOR_BRIDGE_2_IAT_MODE")
+  tor_options+=(--Bridge "obfs4 $TOR_BRIDGE_3_IP_PORT $TOR_BRIDGE_3_ID cert=$TOR_BRIDGE_3_CERT iat-mode=$TOR_BRIDGE_3_IAT_MODE")
+fi
+
+tor "${tor_options[@]}"
